d5678ef27ee4a13f7f85acc668eae5c083be76a3
水墨青花 <1550518923@qq.com>
2020-03-28 20:05:36 +0800

    解决gateway的跨域问题和增加前缀 api/ 的问题

--- cloud-gateway-server/src/main/java/com/wxw/cloud/config/CorsConfig.java
+++ cloud-gateway-server/src/main/java/com/wxw/cloud/config/CorsConfig.java
@@ -1,8 +1,40 @@
 package com.wxw.cloud.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+
 /**
  * @Author: wxw
  * @create: 2020-03-28-1:55
+ *  gateway跨域配置
  */
+@Configuration
 public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        //1.添加CORS配置信息
+        CorsConfiguration config = new CorsConfiguration();
+        //1) 允许的域,不要写*，否则cookie就无法使用了
+        config.addAllowedOrigin("http://manage.wxw.com");
+        config.addAllowedOrigin("http://www.wxw.com");
+        config.addAllowedOrigin("http://wxw.com");
+        //2) 是否发送Cookie信息
+        config.setAllowCredentials(true);
+        //3) 允许的请求方式  OPTIONS/HEAD/GET/PUT/POST/DELETE/PATCH
+        config.addAllowedMethod("*");
+        // 4）允许的头信息
+        config.addAllowedHeader("*");
+
+        //2.添加映射路径，我们拦截一切请求
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**", config);
+
+        //3.返回新的CorsWebFilter
+        return new CorsWebFilter(configSource);
+    }
 }
