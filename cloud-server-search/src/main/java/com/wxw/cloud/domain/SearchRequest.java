709b0b57662ba6b5e4c3691ee3285a7ee9244626
水墨青花 <1550518923@qq.com>
2020-04-15 22:30:08 +0800

    跳过规格参数聚合和数据过滤

--- cloud-server-search/src/main/java/com/wxw/cloud/domain/SearchRequest.java
+++ cloud-server-search/src/main/java/com/wxw/cloud/domain/SearchRequest.java
@@ -1,17 +1,14 @@
 package com.wxw.cloud.domain;
 
-import java.util.Map;
-
 /**
  * @Author: wxw
  * @create: 2020-04-12-21:36
  */
 public class SearchRequest {
+
     private String key;// 搜索条件
 
     private Integer page;// 当前页
-
-    private Map<String,Object> filter;  //规格参数过滤条件
 
     private static final Integer DEFAULT_SIZE = 20;// 每页大小，不从页面接收，而是固定大小
     private static final Integer DEFAULT_PAGE = 1;// 默认页
@@ -22,13 +19,6 @@ public class SearchRequest {
 
     public void setKey(String key) {
         this.key = key;
-    }
-    public Map<String, Object> getFilter() {
-        return filter;
-    }
-
-    public void setFilter(Map<String, Object> filter) {
-        this.filter = filter;
     }
 
     public Integer getPage() {
